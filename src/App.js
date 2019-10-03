86a6ca5a226ed75ab0d324df7ed77fd2be357b57
Giancarlo Vera <gianverab@gmail.com>
2019-10-03 15:40:40 -0500

    get the recipe api with useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
 
 const App = () => {
+  const APP_ID = 'd0bc38cd';
+  const APP_KEY = 'bd4801301fba39c2302d7eb6d3ca4413';
+
+  const API = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const getRecipes = async () => {
+    const response = await fetch(API);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
   return (
-    <div className="App">
+    <Wrapper>
       <h1>Hello Recipe App !!</h1>
-    </div>
+    </Wrapper>
   );
 };
 
 export default App;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  text-align: center;
+`;
